23ffb1c7435dcff2bc496f05ee65266847c91422
Fraser Samson <2661506S@student.gla.ac.uk>
2025-02-24 15:33:22 +0000

    Remove type checking

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import { useEffect, useState } from "react";
 import StoreItemList from "../components/StoreItemList/StoreItemList";
 import { ScrollDirection, StoreItemObj } from "../types/interfaces";
